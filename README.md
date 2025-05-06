990b16ad4b643a5403a6138a2f2fa7d11367bca0
Michael Chavinda <mschavinda@gmail.com>
2025-05-05 17:42:53 -0700

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ihaskell-dataframe
-Displaying DataFrames in iHaskell
---
+++ src/IHaskell/Display/Dataframe.hs
@@ -0,0 +1,16 @@
+module IHaskell.Display.Dataframe where
+
+import           IHaskell.Display
+
+import qualified DataFrame as D
+import DataFrame (DataFrame)
+
+import qualified Data.Text as T
+
+instance IHaskellDisplay DataFrame where
+  display val = return $ Display [stringDisplay, markdownDisplay]
+    where
+      str = T.unpack (D.asText val True)
+      stringDisplay = plain str
+      markdownDisplay = markdown str
+
