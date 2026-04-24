ac8f3ff45cde60ee4d2659af12f343636734ffa6
Michael Chavinda <mschavinda@gmail.com>
2026-04-23 22:34:04 -0700

    Add EmptyCase and DataKinds extensions

--- rc.hs
+++ rc.hs
@@ -1,17 +1,21 @@
 :set -XOverloadedStrings
 :set -XTypeApplications
 :set -XTemplateHaskell
+:set -XEmptyCase
+:set -XDataKinds
 
 import qualified Data.Text as T
-import           DataFrame.Functions (declareColumns)
+import           DataFrame.Functions (declareColumns, declareColumnsWithPrefix)
 import           Data.Text (Text)
-import           DataFrame ((|>))
+import           DataFrame.Operators
 import           Data.Int
 import           Data.Time
 
 import qualified DataFrame as D
 import qualified DataFrame.Functions as F
-import           DataFrame.Functions ((.=), (.==), (.<=), (.>=), (.<), (.>), (.&&), (.||), as, (./=))
+import qualified DataFrame.Display.Web.Plot as Plt
+import qualified DataFrame.Typed as DT
+import           DataFrame.Monad
 
 import GHC.Conc
 
@@ -19,9 +23,3 @@ n <- getNumProcessors
 setNumCapabilities $ max 1 (min 4 (n-1))
 
 default (Int, T.Text, Double)
-
-:def! declareColumns \s -> return ("_ = (); declareColumns " ++ s)
-:set prompt "\ESC[34m\STXdataframe> \ESC[m\STX"
-
-__brightBlue s = "\ESC[94m" ++ s ++ "\ESC[0m"
-__brightGreen s = "\ESC[92m" ++ s ++ "\ESC[0m"
